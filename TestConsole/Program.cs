a4e02488144f1179c41c5d4de703878c8ee927e1
dhutchinson5 <dhutchinson@walsallcollege.ac.uk>
2013-10-21 10:54:15 +0100

    Added Comments

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -10,7 +10,9 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            // Generate Random Number
             var number = new Random((int)DateTime.Now.Ticks).Next(0, 500);
+            // Write Random Number
             Console.WriteLine(number);
         }
     }
